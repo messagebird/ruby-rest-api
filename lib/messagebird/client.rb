8b16d17554829cde6ce6830f9b70cd9aa9349ea2
Vincent Boisard <v.boisard@catawiki.nl>
2017-01-09 14:06:50 +0100

    fix: use actual exception class when MessageBird replies with an unexpected status code

--- lib/messagebird/client.rb
+++ lib/messagebird/client.rb
@@ -51,7 +51,7 @@ module MessageBird
       when 200, 201, 204, 401, 404, 405, 422
         json = JSON.parse(response.body)
       else
-        raise Net::HTTPServerError.new response.http_version, 'Unknown response from server', response
+        raise Net::HTTPServerException.new 'Unknown response from server', response
       end
 
       # If the request returned errors, create Error objects and raise.
@@ -141,7 +141,7 @@ module MessageBird
 
     def lookup(phoneNumber, params={})
       Lookup.new(request(:get, "lookup/#{phoneNumber}", params))
-    end 
+    end
 
     def lookup_hlr_create(phoneNumber, params={})
       HLR.new(request(:post, "lookup/#{phoneNumber}/hlr", params))

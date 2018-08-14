5231f5fd67814c7b2607752cbdc01adec3cdff44
Emile Pels <emile@messagebird.com>
2018-08-14 15:32:15 +0200

    Add balance test

---
+++ spec/balance_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'rspec/mocks'
+require 'messagebird'
+require 'uri'
+
+describe 'Balance' do
+
+  it 'parses the balance' do
+
+    http_client = double(MessageBird::HttpClient)
+    client = MessageBird::Client.new('', http_client)
+
+    expect(http_client)
+        .to receive(:request)
+        .with(:get, 'balance', {})
+        .and_return('{"payment":"prepaid","type":"credits","amount":12.34}')
+
+    balance = client.balance
+
+    expect(balance.payment).to eq 'prepaid'
+
+  end
+
+end

844b01ecfb3c3a12304807c9e33260fcaf55e8d6
Emile Pels <emile@messagebird.com>
2018-08-17 13:56:25 +0200

    Add Contact objects

---
+++ lib/messagebird/custom_details.rb
@@ -0,0 +1,6 @@
+require 'messagebird/base'
+
+class CustomDetails < MessageBird::Base
+  # CustomDetails holds free-input fields for the Contact object.
+  attr_accessor :custom1, :custom2, :custom3, :custom4
+end

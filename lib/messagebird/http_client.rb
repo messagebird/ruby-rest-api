1f99e0b4caf7b0fe8064893a78dabdd127ba5a2f
Emile Pels <emile@messagebird.com>
2018-08-17 12:19:48 +0200

    Extract request preparation to separate method

--- lib/messagebird/http_client.rb
+++ lib/messagebird/http_client.rb
@@ -22,8 +22,33 @@ module MessageBird
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl     = true
 
-      # Construct the HTTP GET or POST request.
+      request = prepare_request(method, uri, params)
+
+      # Execute the request and fetch the response.
+      response = http.request(request)
+
+      # Parse the HTTP response.
+      case response.code.to_i
+      when 200, 201, 204, 401, 404, 405, 422
+        # Ok
+      else
+        raise InvalidPhoneNumberException, 'Unknown response from server'
+      end
+
+      # Execute the request and fetch the response.
+      response = http.request(request)
+
+      assert_valid_response_code(response.code.to_i)
+      assert_json_response_type(response['Content-Type']) unless check_json
+
+      response.body
+    end
+
+    def prepare_request(method, uri, params={})
+      # Construct the HTTP request.
       case method
+      when :delete
+        request = Net::HTTP::Delete.new(uri.request_uri)
       when :get
         request = Net::HTTP::Get.new(uri.request_uri)
       when :post
@@ -36,16 +61,9 @@ module MessageBird
       request['Authorization'] = "AccessKey #{@access_key}"
       request['User-Agent']    = "MessageBird/ApiClient/#{CLIENT_VERSION} Ruby/#{RUBY_VERSION}"
 
-      # If present, add the HTTP POST parameters.
       request.set_form_data(params) if method == :post && !params.empty?
 
-      # Execute the request and fetch the response.
-      response = http.request(request)
-
-      assert_valid_response_code(response.code.to_i)
-      assert_json_response_type(response['Content-Type']) unless check_json
-
-      response.body
+      request
     end
 
     # Throw an exception if the response code is not one we expect from the

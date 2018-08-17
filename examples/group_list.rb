b8cc15c40ffcf842e5fd0bb6241e7ff57504fd04
Emile Pels <emile@messagebird.com>
2018-08-17 16:05:26 +0200

    Add examples

---
+++ examples/group_list.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+$:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib/')
+require 'messagebird'
+
+ACCESS_KEY = 'YOUR KEY HERE'
+
+unless defined?(ACCESS_KEY)
+  puts 'You need to set an ACCESS_KEY constant in this file'
+  exit 1
+end
+
+begin
+  # Create a MessageBird client with the specified ACCESS_KEY.
+  client = MessageBird::Client.new(ACCESS_KEY)
+
+  # Fetch the Group list with pagination options (skip the first 5 objects and take 10).
+  limit = 10
+  offset = 5
+  groups = client.group_list(limit, offset)
+
+  # Print the object information.
+  puts
+  puts "The following information was returned as a Group list:"
+  puts
+  puts "  count           : #{groups.count}"
+  puts "  limit           : #{groups.limit}"
+  puts "  offset          : #{groups.offset}"
+  puts "  totalCount      : #{groups.totalCount}"
+  puts "  links           : #{groups.links}"
+
+  unless groups.items.empty?
+    group = groups[0] # Equivalent to groups.items[0]
+
+    puts "  Group             :"
+    puts "    id              : #{group.id}"
+    puts "    href            : #{group.href}"
+    puts "    name            : #{group.name}"
+    puts "    contacts        : #{group.contacts.href}"
+  end
+
+rescue MessageBird::ErrorException => ex
+  puts
+  puts 'An error occurred while listing your groups:'
+  puts
+
+  ex.errors.each do |error|
+    puts "  code        : #{error.code}"
+    puts "  description : #{error.description}"
+    puts "  parameter   : #{error.parameter}"
+    puts
+  end
+end

f2d9142701606e5f077920b5c19bb41141a2f3c9
Emile Pels <emile@messagebird.com>
2018-09-16 14:18:01 +0200

    Bump version to 1.4.1 (patch)

--- lib/messagebird.rb
+++ lib/messagebird.rb
@@ -2,7 +2,7 @@ libdir = File.dirname(__FILE__)
 $:.unshift(libdir) unless $:.include?(libdir)
 
 module MessageBird
-  CLIENT_VERSION = '1.4.0'
+  CLIENT_VERSION = '1.4.1'
   ENDPOINT       = 'https://rest.messagebird.com'
 end
 

9a72f49a5db6f5c5876b977a81b5821db9ec9b70
Joris Machielse <joris@machielse.org>
2018-10-04 19:45:10 -0400

    Wrapped List class inside MessageBird namespace

--- lib/messagebird/list.rb
+++ lib/messagebird/list.rb
@@ -1,23 +1,23 @@
 require 'messagebird/base'
 
-class List < MessageBird::Base
+module MessageBird
+  class List < MessageBird::Base
+    attr_accessor :offset, :limit, :count, :totalCount, :links, :items
 
-  attr_accessor :offset, :limit, :count, :totalCount, :links, :items
+    # type will be used to create objects for the items, e.g.
+    # List.new(Contact, {}).
+    def initialize(type, json)
+      @type = type
 
-  # type will be used to create objects for the items, e.g.
-  # List.new(Contact, {}).
-  def initialize(type, json)
-    @type = type
+      super(json)
+    end
 
-    super(json)
+    def items=(value)
+      @items = value.map { |i| @type.new i }
+    end
+
+    def [](index)
+      @items[index]
+    end
   end
-
-  def items=(value)
-    @items = value.map { |i| @type.new i }
-  end
-
-  def [](index)
-    @items[index]
-  end
-
 end

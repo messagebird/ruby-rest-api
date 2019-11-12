38eec25eb8cb32d625b8dc4f204c548dd26455fd
Alireza Eliaderani <eliaderani@gmail.com>
2019-11-12 10:33:01 +0100

    Add callFlow to Call object

--- lib/messagebird/call.rb
+++ lib/messagebird/call.rb
@@ -3,7 +3,7 @@ require 'messagebird/call/webhook'
 
 module MessageBird
   class Call < MessageBird::Base
-    attr_accessor :id, :status, :source, :destination, :createdAt, :updatedAt, :endedAt, :webhook
+    attr_accessor :id, :status, :source, :destination, :createdAt, :updatedAt, :endedAt, :webhook, :callFlow
     def initialize(json)
       params = json.include?("data") ? json["data"].first : json
       super(params)

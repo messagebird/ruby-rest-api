13d36405ee671b9c92bf65e9276509e8deec6cdc
Mathieu Kooiman <mathieu@Mathieus-MacBook-Pro.local>
2019-12-04 15:03:10 +0100

    Add examples for calls, call legs and recording retrieval

---
+++ examples/call.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+$:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib/')
+require 'messagebird'
+
+ACCESS_KEY = 'YOUR ACCESS KEY HERE'
+
+unless defined?(ACCESS_KEY)
+  puts 'You need to set an ACCESS_KEY constant in this file'
+  exit 1
+end
+
+begin
+  # Create a MessageBird client with the specified ACCESS_KEY.
+  client = MessageBird::Client.new(ACCESS_KEY)
+
+  # Start a conversation
+  calls = client.call_list(10, 0)
+
+  calls.items.each do |call|
+    puts "Call ID: #{call.id}"
+  end
+
+rescue MessageBird::ErrorException => ex
+  puts
+  puts 'An error occured while listing the calls:'
+  puts
+
+  ex.errors.each do |error|
+    puts "  code        : #{error.code}"
+    puts "  description : #{error.description}"
+    puts "  parameter   : #{error.parameter}"
+    puts
+  end
+end

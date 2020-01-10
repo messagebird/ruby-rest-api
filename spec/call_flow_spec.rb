04a79e2dfca1a85fca1130167b026e1eb8eb9885
deepak borania <deepak.borania@messagebird.com>
2020-01-10 12:13:03 +0100

    Fixing some style issues

--- spec/call_flow_spec.rb
+++ spec/call_flow_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe 'CallFlow' do
-  
+
   let(:voice_client) { double(MessageBird::VoiceClient) }
   let(:client) { MessageBird::Client.new('', nil, nil, voice_client) }
   let(:call_flow_id) { '7355a691-381d-42b0-a037-9df5dab19e8e' }
@@ -9,15 +9,15 @@ describe 'CallFlow' do
   let(:title) { '' }
   let(:record) { true }
   let(:default) { false }
-  
+
   it 'creates a call flow' do
     steps = [
       {
         action: 'transfer',
-        options:  {
-          destination: '31612345678',
+        options: {
+          destination: '31612345678'
         },
-      },
+      }
     ]
 
     mock_data = {
@@ -28,23 +28,24 @@ describe 'CallFlow' do
           steps: [
             {
               id: step_id,
-              action: 'transfer', 
+              action: 'transfer',
               options: {
                 destination: '31612345678'
               }
             }
           ],
-          record: true, 
-          default: false, 
-          createdAt: '2019-11-07T12:37:59Z', 
+          record: true,
+          default: false,
+          createdAt: '2019-11-07T12:37:59Z',
           updatedAt: '2019-11-07T12:37:59Z'
         }
       ]
     }
-    expect(voice_client).to receive(:request)
-                              .with(:post, 'call-flows', { title: title, steps: steps, default: default, record: record })
-                              .and_return(mock_data.to_json)
-                              
+    expect(voice_client)
+      .to receive(:request)
+      .with(:post, 'call-flows', title: title, steps: steps, default: default, record: record )
+      .and_return(mock_data.to_json)
+
     call_flow = client.call_flow_create(title, steps, default, record)
     expect(call_flow.id).to eq call_flow_id
   end
@@ -61,15 +62,15 @@ describe 'CallFlow' do
           steps: [
             {
               id: 'sf001',
-              action: 'transfer', 
+              action: 'transfer',
               options: {
                 destination: '31612345678'
               }
             }
           ],
-          record: true, 
-          default: false, 
-          createdAt: '2019-11-07T12:37:59Z', 
+          record: true,
+          default: false,
+          createdAt: '2019-11-07T12:37:59Z',
           updatedAt: '2019-11-07T12:37:59Z'
         },
         {
@@ -78,23 +79,24 @@ describe 'CallFlow' do
           steps: [
             {
               id: 's000000000',
-              action: 'transfer', 
+              action: 'transfer',
               options: {
                 destination: '31612345678'
               }
             }
           ],
-          record: true, 
-          default: false, 
-          createdAt: '2019-11-07T12:37:59Z', 
+          record: true,
+          default: false,
+          createdAt: '2019-11-07T12:37:59Z',
           updatedAt: '2019-11-07T12:37:59Z'
         }
       ]
     }
 
-    expect(voice_client).to receive(:request)
-                              .with(:get, 'call-flows?perPage=2&page=1', {})
-                              .and_return(mock_data.to_json)
+    expect(voice_client)
+      .to receive(:request)
+      .with(:get, 'call-flows?perPage=2&page=1', {})
+      .and_return(mock_data.to_json)
     list = client.call_flow_list(2, 1)
 
     expect(list.items.count).to eq 2
@@ -113,23 +115,24 @@ describe 'CallFlow' do
           steps: [
             {
               id: 'sfid-1',
-              action: 'transfer', 
+              action: 'transfer',
               options: {
                 destination: '31612345678'
               }
             }
           ],
-          record: true, 
-          default: false, 
-          createdAt: '2019-11-07T12:37:59Z', 
+          record: true,
+          default: false,
+          createdAt: '2019-11-07T12:37:59Z',
           updatedAt: '2019-11-07T12:37:59Z'
         }
       ]
     }
 
-    expect(voice_client).to receive(:request)
-                              .with(:get, 'call-flows/cfid-1', {})
-                              .and_return(mock_data.to_json)
+    expect(voice_client)
+      .to receive(:request)
+      .with(:get, 'call-flows/cfid-1', {})
+      .and_return(mock_data.to_json)
     call_flow = client.call_flow_view('cfid-1')
     expect(call_flow.id).to eq 'cfid-1'
   end
@@ -138,9 +141,10 @@ describe 'CallFlow' do
     voice_client = double(MessageBird::VoiceClient)
     client = MessageBird::Client.new('', nil, nil, voice_client)
 
-    expect(voice_client).to receive(:request)
-                              .with(:delete, 'call-flows/cfid-1', {})
-                              .and_return('')
-    call_flow = client.call_flow_delete('cfid-1')
+    expect(voice_client)
+      .to receive(:request)
+      .with(:delete, 'call-flows/cfid-1', {})
+      .and_return('')
+    client.call_flow_delete('cfid-1')
   end
 end

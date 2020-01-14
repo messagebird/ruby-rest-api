7f6cb78a6c7937b678af9c9e5ab13277c253418b
Jalerson Lima <jalerson.lima@messagebird.com>
2020-01-14 11:29:02 +0100

    Replace by snake case

--- lib/messagebird/number.rb
+++ lib/messagebird/number.rb
@@ -3,14 +3,14 @@ require 'messagebird/base'
 module MessageBird 
   class Number < MessageBird::Base
     attr_accessor :number, :country, :region, :locality, :features, :tags, :type, :status
-    attr_reader :createdAt, :renewalAt
+    attr_reader :created_at, :renewal_at
     
-    def createdAt=(value)
-      @createdAt = value_to_time(value)
+    def created_at=(value)
+      @created_at = Time.parse(value)
     end
 
-    def renewalAt=(value)
-      @renewalAt = value_to_time(value)
+    def renewal_at=(value)
+      @renewal_at = Time.parse(value)
     end
   end 
 end 

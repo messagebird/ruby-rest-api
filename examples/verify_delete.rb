736c289009baf8c0da24d1347ae72bb23d890268
Gabriel Oliveira <gabriel.so@gmail.com>
2021-06-25 09:59:50 +0200

    Addressing rubocop

--- examples/verify_delete.rb
+++ examples/verify_delete.rb
@@ -13,30 +13,30 @@ unless defined?(ACCESS_KEY)
 end
 
 unless defined?(VERIFY_ID)
-    puts 'You need to set a VERIFY_ID constant in this file'
-    exit 1
-  end
+  puts 'You need to set a VERIFY_ID constant in this file'
+  exit 1
+end
 
 begin
-    # Create a MessageBird client with the specified ACCESS_KEY.
-    client = MessageBird::Client.new(ACCESS_KEY)
-  
-    # Delete the verify
-    client.verify_delete(VERIFY_ID)
-  
-    # Print the object information.
+  # Create a MessageBird client with the specified ACCESS_KEY.
+  client = MessageBird::Client.new(ACCESS_KEY)
+
+  # Delete the verify
+  client.verify_delete(VERIFY_ID)
+
+  # Print the object information.
+  puts
+  puts 'Success. The delete method has empty return.'
+  puts
+rescue MessageBird::ErrorException => e
+  puts
+  puts 'An error occured while requesting an OTP object:'
+  puts
+
+  e.errors.each do |error|
+    puts "  code        : #{error.code}"
+    puts "  description : #{error.description}"
+    puts "  parameter   : #{error.parameter}"
     puts
-    puts 'Success. The delete method has empty return.'
-    puts
-  rescue MessageBird::ErrorException => e
-    puts
-    puts 'An error occured while requesting an OTP object:'
-    puts
-  
-    e.errors.each do |error|
-      puts "  code        : #{error.code}"
-      puts "  description : #{error.description}"
-      puts "  parameter   : #{error.parameter}"
-      puts
-    end
   end
+end

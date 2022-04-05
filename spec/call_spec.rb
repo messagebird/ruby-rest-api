eee307819a48a5ee5aed1c1c47a73c8bbdd6f9ff
marcel corso <marcel@messagebird.com>
2022-04-05 11:21:42 +0200

    rubocop doesnt like %Q

--- spec/call_spec.rb
+++ spec/call_spec.rb
@@ -39,7 +39,7 @@ describe 'Call' do
     expect(voice_client)
       .to receive(:request)
       .with(:post, 'calls', { source: source, destination: destination, callFlow: call_flow.to_json })
-      .and_return(%Q({"data":[{"id":"#{call_id}","status":"queued","source":"#{source}","destination":"#{destination}","createdAt":"2019-10-11T13:02:19Z","updatedAt":"2019-10-11T13:02:19Z","endedAt":null}],"_links":{"self":"/calls/#{call_id}"},"pagination":{"total_count":0,"pageCount":0,"currentPage":0,"perPage":0}}))
+      .and_return(%({"data":[{"id":"#{call_id}","status":"queued","source":"#{source}","destination":"#{destination}","createdAt":"2019-10-11T13:02:19Z","updatedAt":"2019-10-11T13:02:19Z","endedAt":null}],"_links":{"self":"/calls/#{call_id}"},"pagination":{"total_count":0,"pageCount":0,"currentPage":0,"perPage":0}}))
     call = client.call_create(source, destination, call_flow)
     expect(call.id).to eq call_id
   end
